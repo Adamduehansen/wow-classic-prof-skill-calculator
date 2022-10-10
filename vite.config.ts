9914901f35d0c4f3267b062fdac0e5ee7b4908f0
Adam Due Hansen <adamduehan@gmail.com>
2022-10-10 07:52:13 +0200

    Adds deployment to Github pages.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+export default defineConfig(({ command }) => {
+  if (command === 'serve') {
+    return {
+      plugins: [react()],
+    };
+  } else {
+    return {
+      base: '/wow-classic-prof-skill-calculator/',
+      plugins: [react()],
+    };
+  }
+});
